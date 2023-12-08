2500a8e0a0e41771bdd6c537f5bf16e02dadf021
Thanet <Thanet@Lenovo-PC>
2023-12-08 09:19:33 -0700

    adding substraction

--- src/main/java/com/java/basic/SubNum.java
+++ src/main/java/com/java/basic/SubNum.java
@@ -5,6 +5,7 @@ public class SubNum {
 	public static void main(String[] args) {
 		System.out.println(40-30);
         System.out.println(60-20);
+        System.out.println("My name is shiva");
 	}
 
 }
